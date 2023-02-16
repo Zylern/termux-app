ecf3cdab5e755ad940599b8b1948eee0f436631c
Zylern <coolzylern@gmail.com>
2023-02-16 12:45:18 +0530

    Add files via upload

--- app/src/main/java/com/termux/app/activities/SettingsActivity.java
+++ app/src/main/java/com/termux/app/activities/SettingsActivity.java
@@ -167,13 +167,10 @@ public class SettingsActivity extends AppCompatActivity {
         }
 
         private void configureZylernPreference(@NonNull Context context) {
-            Preference donatePreference = findPreference("zylern");
+            Preference zylernPreference = findPreference("zylern");
             if (zylernPreference != null) {
                 String signingCertificateSHA256Digest = PackageUtils.getSigningCertificateSHA256DigestForPackage(context);
                 if (signingCertificateSHA256Digest != null) {
-                    // If APK is a Google Playstore release, then do not show the donation link
-                    // since Termux isn't exempted from the playstore policy donation links restriction
-                    // Check Fund solicitations: https://pay.google.com/intl/en_in/about/policy/
                     String apkRelease = TermuxUtils.getAPKRelease(signingCertificateSHA256Digest);
                     if (apkRelease == null || apkRelease.equals(TermuxConstants.APK_RELEASE_GOOGLE_PLAYSTORE_SIGNING_CERTIFICATE_SHA256_DIGEST)) {
                         zylernPreference.setVisible(false);
